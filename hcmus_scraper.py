d37b9df69fb090fc962b2a155f8926b52019a56d
kmq-mqk <khucminhquan2304@gmail.com>
2025-05-05 12:21:34 +0700

    hcmus_scraper can interact with hcmus.txt now, little optimization

--- hcmus_scraper.py
+++ hcmus_scraper.py
@@ -2,6 +2,45 @@
 import requests
 from bs4 import BeautifulSoup
 
+# write procedure
+def writeFile(targetBlock):
+  # write file
+  fout = open('hcmus.txt', 'w')
+
+  # to check if there is new post later
+  fout.write(f"{targetBlock.article['id']}\r\n\r\n\r\n")
+  # get info relate to students
+
+  fout.write(f"{border} THONG TIN SV {border}\r\n")
+
+  # targetBlock = soup.find("div", class_="cmsmasters_archive")
+
+
+
+  articles = targetBlock.find_all("article")
+
+  for article in articles:
+    content = article.find("div", class_="cmsmasters_archive_item_cont_wrap")
+    data = content.header.h2.a
+    date = content.footer.span.abbr
+
+    fout.write(f"{date.string} _ [ {data.string.get_text(strip=True)} ]\r\n{data['href']}\r\n\r\n")
+
+  # get newest posts
+
+  fout.write(f"\r\n{border} THONG TIN MOI NHAT {border}\r\n")
+
+  targetBlock = soup.find(string="Bài viết mới")
+
+  targetBlock = targetBlock.parent.parent
+  targetBlock = targetBlock.ul
+
+  data = targetBlock.find_all("li")
+
+  for post in data:
+    fout.write(f"{post.span.string} _ [ {post.a.string} ]\r\n{post.a['href']}\r\n\r\n")
+
+# main
 targetUrl = "https://hcmus.edu.vn/category/dao-tao/dai-hoc/thong-tin-danh-cho-sinh-vien/"
 
 headers = {
@@ -15,41 +54,19 @@ number = 17
 ch = "*"
 border = ch * number
 
-# read file
-fin = open('hcmus.txt', 'r')
-
-
-
-# write file
-fout = open('hcmus.txt', 'w')
-
-# get newest posts
-
-fout.write(f"{border} THONG TIN MOI NHAT {border}\r\n")
-
-targetBlock = soup.find(string="Bài viết mới")
-
-targetBlock = targetBlock.parent.parent
-targetBlock = targetBlock.ul
-
-data = targetBlock.find_all("li")
-
-for post in data:
-  fout.write(f"{post.span.string} _ [ {post.a.string} ]\r\n{post.a['href']}\r\n\r\n")
-
-# get info relate to students
-
-fout.write(f"{border} THONG TIN SV {border}\r\n")
-
 targetBlock = soup.find("div", class_="cmsmasters_archive")
 
-fout.write(f"Newest post's id = {targetBlock.article['id']}\r\n\r\n\r\n")
+# read file
+fin = open('hcmus.txt', 'r')
+newest = fin.readline().strip()
+# if the file is empty
+if newest == '':
+  writeFile(targetBlock)
+else:
+  nowNewest = targetBlock.article['id']
 
-articles = targetBlock.find_all("article")
-
-for article in articles:
-  content = article.find("div", class_="cmsmasters_archive_item_cont_wrap")
-  data = content.header.h2.a
-  date = content.footer.span.abbr
-
-  fout.write(f"{date.string} _ [ {data.string.get_text(strip=True)} ]\r\n{data['href']}\r\n\r\n")
+  if newest != nowNewest:
+    print("Have to update!")
+    writeFile(targetBlock)
+  else:
+    print("Already up to date!")
